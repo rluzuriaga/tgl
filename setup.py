c00c33b4fe9f83a950be8bf7446fd48d5b397007
rluzuriaga <rodrigoluzuriaga@icloud.com>
2020-07-01 12:39:12 -0700

    Add classifiers and package data to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='0.0.1',
     zip_safe=False,
     packages=find_packages(),
+    package_data={'togglcli': ['data/*.*']},
     entry_points={
         'console_scripts': [
             'togglcli = togglcli.main:setuptools_entry',
@@ -12,5 +13,16 @@ setup(
     },
     install_requires = [
         'requests>=2.23',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Utilities',
+        'Typing :: Typed'
     ]
 )
