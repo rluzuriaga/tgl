cc2cd5bdfb21726ee2eb01b7b4e69b0476a180d4
rluzuriaga <rodrigoluzuriaga@icloud.com>
2021-03-16 14:29:24 -0700

    Create test for bad email address for setup config. #13

--- tests/setup_command_tests.py
+++ tests/setup_command_tests.py
@@ -19,10 +19,16 @@ class TestSetupCommand(unittest.TestCase):
         return cmd.before.decode('utf-8')
 
     def test_empty_setup_config(self) -> None:
-        """ Simulate the user pressing enter for both email and password instead of entering the needed data. """
+        """ Test the output when the user doesn't enter anything (presses Enter) for email and password. """
         output = self._setup_command('\n', '\n')
 
         self.assertIn('Nothing entered, closing program.', output)
+
+    def test_bad_email(self) -> None:
+        """ Test the output when the user enters an incorrect email. """
+        output = self._setup_command('this_is_a_bad_email@bad_email.com', '\n')
+
+        self.assertIn('Error: Incorrect credentials.', output)
 
 
 if __name__ == "__main__":
