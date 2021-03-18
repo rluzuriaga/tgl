33f49612b3accdd2dcb15dd8c527bd31c0451446
rluzuriaga <rodrigoluzuriaga@icloud.com>
2021-03-18 12:02:20 -0700

    Create test for empty start command. #13

--- tests/start_command_tests.py
+++ tests/start_command_tests.py
@@ -1,4 +1,7 @@
 import unittest
+from signal import SIG_DFL
+
+import pexpect
 
 from tgl.utils import delete_user_data
 
@@ -11,6 +14,18 @@ class TestStartCommand(unittest.TestCase):
     def tearDown(self) -> None:
         delete_user_data()
         return super().tearDown()
+
+    def test_empty_start_command(self) -> None:
+        """ Test the output of and empty start command. """
+        cmd = pexpect.spawn('tgl start')
+        cmd.expect(pexpect.EOF)
+        cmd.kill(SIG_DFL)
+        cmd.close()
+
+        output = cmd.before.decode('utf-8')
+
+        self.assertIn('usage: tgl start [-h] [-p] [-t [TAGS [TAGS ...]]] [-w] [-b] description', output)
+        self.assertIn('tgl start: error: the following arguments are required: description', output)
 
 
 if __name__ == "__main__":
