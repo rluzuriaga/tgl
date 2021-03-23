a874c05b2f23458e5c7b159932767af14bfd4e9d
rluzuriaga <rodrigoluzuriaga@icloud.com>
2021-03-23 12:34:00 -0700

    Remove tests utils.py and add the function to the help messages tests

--- tests/help_messages_tests.py
+++ tests/help_messages_tests.py
@@ -1,6 +1,16 @@
 import unittest
+import subprocess
 
-from tests.utils import run_command
+
+def run_command(cmd: str, error: bool = False) -> str:
+    output = subprocess.run(cmd.split(), stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+
+    output_string = output.stdout.decode('utf-8')
+
+    if error:
+        output_string = output.stderr.decode('utf-8')
+
+    return output_string
 
 
 class TestHelpMessages(unittest.TestCase):
