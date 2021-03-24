4a44f5b76aafd3ba73a3fde1fb87257ac2181eb0
rluzuriaga <rodrigoluzuriaga@icloud.com>
2021-03-24 15:14:50 -0700

    Create tests for stop command with no timer running. #13

---
+++ tests/stop_command_tests.py
@@ -0,0 +1,25 @@
+import unittest
+
+from tgl.utils import delete_user_data
+from tests.utils import setup_credentials, run_command
+
+
+class TestStopCommand(unittest.TestCase):
+    def setUp(self) -> None:
+        delete_user_data()
+        setup_credentials()
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        delete_user_data()
+        return super().tearDown()
+
+    def test_stop_command_without_any_timer_running(self) -> None:
+        """ Test the output of the stop command without any timer running. """
+        output = run_command('tgl stop')
+
+        self.assertIn('There is no timer currently running.', output)
+
+
+if __name__ == "__main__":
+    unittest.main()
